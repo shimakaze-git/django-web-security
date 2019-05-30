474ee49be9b6e5b024912865a4666af7fd104650
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-05-30 19:15:38 +0900

    feat: add fixture datas

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
     'sushi.apps.SushiConfig'
 ]
 
