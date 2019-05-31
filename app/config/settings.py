5b2069368a2fa88c1d141e9a6d697d38f57117d2
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-06-01 03:30:10 +0900

    fix: fix settings.py DATABASES

--- app/config/settings.py
+++ app/config/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    # 'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
